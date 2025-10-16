4453878456f9c5e31b9afc990f7ffee3a7575e75
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-10-16 20:25:08 +0300

    init

---
+++ build/CMakeFiles/AuebComputerGraphicsCourse.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AuebComputerGraphicsCourse.dir/link.d"
+  "AuebComputerGraphicsCourse"
+  "AuebComputerGraphicsCourse.pdb"
+  "CMakeFiles/AuebComputerGraphicsCourse.dir/src/main.cpp.o"
+  "CMakeFiles/AuebComputerGraphicsCourse.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AuebComputerGraphicsCourse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
